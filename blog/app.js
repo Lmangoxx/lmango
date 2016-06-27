2c01dc4680dcd945105bd32054747feaf5eccee8
hanxueliang <hanxueliang@ggxueche.com>
2016-06-27 13:31:39 +0800

    第三方静态资源目录更换

--- blog/app.js
+++ blog/app.js
@@ -1,11 +1,12 @@
 var express = require('express');  //引入express
-var path = require('path');  //引入
+var path = require('path');  //引入path
 var port = process.env.PORT || 3000;   //端口设置（process.env.PORT在shell环境下设置端口）
 var app = express();  //设置服务
 
 app.set('view engine','jade');  //设置默认的模板引擎
-app.set('views','./views/pages');   //设置视图的根目录
-app.use(express.static(path.join(__dirname,'bower_components')));  //设置静态资源目录(css,js,images等)
+app.set('views','./views/pages');   //设置视图的根目录(jade文件目录)
+app.use('/lib/',express.static(path.join(__dirname,'public/lib/bower_components/')));  //设置第三方引入静态资源路径
+app.use('/static/',express.static(path.join(__dirname,'public/')));  //设置自己静态资源路径
 app.listen(port);  //监听端口
 
 app.get('/index',function(req,res){
